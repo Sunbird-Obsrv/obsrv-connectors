e8fc52cb48de424739db2b0410693770026105f0
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2023-11-17 14:46:23 +0530

    Issue #4 feat: add obsrv and source meta to ingest event (#3)

--- kafka-connector/src/main/scala/org/sunbird/obsrv/kafkaconnector/task/KafkaConnectorStreamTask.scala
+++ kafka-connector/src/main/scala/org/sunbird/obsrv/kafkaconnector/task/KafkaConnectorStreamTask.scala
@@ -4,14 +4,16 @@ import com.typesafe.config.ConfigFactory
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.datastream.DataStream
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.joda.time.{DateTime, DateTimeZone}
 import org.sunbird.obsrv.core.streaming.{BaseStreamTask, FlinkKafkaConnector}
 import org.sunbird.obsrv.core.util.{FlinkUtil, JSONUtil}
+import org.sunbird.obsrv.model.DatasetModels.DatasetSourceConfig
 import org.sunbird.obsrv.registry.DatasetRegistry
-import org.joda.time.DateTime
-import org.joda.time.DateTimeZone
 
 import java.io.File
+import java.util.UUID
 import scala.collection.mutable
+
 
 class KafkaConnectorStreamTask(config: KafkaConnectorConfig, kafkaConnector: FlinkKafkaConnector) extends BaseStreamTask[String] {
 
@@ -38,6 +40,7 @@ class KafkaConnectorStreamTask(config: KafkaConnectorConfig, kafkaConnector: Fli
               val mutableMap = JSONUtil.deserialize[mutable.Map[String, AnyRef]](streamMap)
               mutableMap.put("dataset", datasetId)
               mutableMap.put("syncts", java.lang.Long.valueOf(new DateTime(DateTimeZone.UTC).getMillis))
+              addObsrvMeta(mutableMap, dataSourceConfig)
               JSONUtil.serialize(mutableMap)
             }
           }.returns(classOf[String])
@@ -47,6 +50,25 @@ class KafkaConnectorStreamTask(config: KafkaConnectorConfig, kafkaConnector: Fli
       }
       env.execute(config.jobName)
     }
+  }
+
+  private def addObsrvMeta(event: mutable.Map[String, AnyRef], dataSourceConfig: DatasetSourceConfig) : Unit ={
+    event.put("obsrv_meta", Map(
+      "syncts" -> System.currentTimeMillis(),
+      "processingStartTime" -> System.currentTimeMillis(),
+      "flags" -> Map(),
+      "timespans" -> Map(),
+      "error" -> Map(),
+      "source" -> Map(
+        "meta" -> Map(
+          "id" -> dataSourceConfig.id,
+          "connector_type" -> "kafka",
+          "version" -> config.connectorVersion,
+          "entry_source" -> dataSourceConfig.connectorConfig.topic
+        ),
+        "trace_id" -> UUID.randomUUID().toString
+      )
+    ))
   }
 
   override def processStream(dataStream: DataStream[String]): DataStream[String] = {
