bc12a515ab94b7d6aae16fbbf69e0be35a985eb2
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2023-12-15 10:08:41 +0530

    Issue #2 fix: update connector to use timestamp column (#6)
    * feat: add jdbc connector
    
    * feat: add jdbc connector
    
    * feat: add jdbc connector
    
    * feat: update jdbc connector
    
    * feat: enhance jdbc connector
    
    * feat: enhance jdbc connector
    
    * feat: update pom file
    
    * feat: update pom file
    
    * Update JDBCConnectorConfig.scala
    
    * Issue #2 feat: add obsrv meta to event
    
    * Issue #2 feat: update batch event
    
    * Issue #2 feat: update connector helper
    
    * Revert "Issue #2 feat: update connector helper"
    
    This reverts commit 9aed78ee09911acd6601421dbe4b7db19bfbb469.
    
    * Revert "Revert "Issue #2 feat: update connector helper""
    
    This reverts commit 3282345d5b1dcd16cd285d8a9481c21a8225f661.
    
    * Issue #2 feat: enhance pulling data from multiple datasets
    
    * Update jdbc-connector/src/main/scala/org/sunbird/obsrv/client/KafkaClient.scala
    
    Co-authored-by: Mahesh Kumar Gangula <mahesh@sanketika.in>
    
    * Issue #2 feat: update connector helper
    
    * Issue #2 feat: enhance connector implementation
    
    * Issue #2 feat: add decryption and db schema update
    
    * Issue #2 feat: remove kafka client and sink to kafka using dataframe
    
    * Issue #2 feat: remove parse records
    
    * Issue #2 feat: remove parse records
    
    * Issue #2 feat: fetch and use timestamp column in the sql query and check active datasets
    
    * Issue #2 feat: add metrics
    
    * Issue #2 feat: add metrics
    
    * Update ConnectorHelper.scala
    
    * Issue #2 feat: enhance exceptions
    
    * Issue #2 feat: add dataset version
    
    * Issue #2 feat: add dataset version
    
    * Issue #2 feat: update metrics config
    
    * Issue #2 fix: update connector to use timestamp column
    
    ---------
    
    Co-authored-by: Mahesh Kumar Gangula <mahesh@sanketika.in>

--- jdbc-connector/src/main/scala/org/sunbird/obsrv/helper/ConnectorHelper.scala
+++ jdbc-connector/src/main/scala/org/sunbird/obsrv/helper/ConnectorHelper.scala
@@ -83,7 +83,7 @@ class ConnectorHelper(config: JDBCConnectorConfig) extends Serializable {
 
   def processRecords(config: JDBCConnectorConfig, dataset: DatasetModels.Dataset, batch: Int, data: DataFrame, dsSourceConfig: DatasetSourceConfig, metrics: MetricsHelper): Unit = {
     val processStartTime = System.currentTimeMillis()
-    val lastRowTimestamp = data.orderBy(data(dataset.datasetConfig.tsKey).desc).first().getAs[Timestamp](dataset.datasetConfig.tsKey)
+    val lastRowTimestamp = data.orderBy(data(dsSourceConfig.connectorConfig.timestampColumn).desc).first().getAs[Timestamp](dsSourceConfig.connectorConfig.timestampColumn)
     val eventCount = data.count()
     pushToKafka(config, dataset, dsSourceConfig, data)
     val eventProcessingTime = System.currentTimeMillis() - processStartTime
@@ -137,9 +137,9 @@ class ConnectorHelper(config: JDBCConnectorConfig) extends Serializable {
 
   private def getQuery(connectorStats: ConnectorStats, connectorConfig: ConnectorConfig, dataset: Dataset, offset: Int): String = {
     if (connectorStats.lastFetchTimestamp == null) {
-      s"SELECT * FROM ${connectorConfig.tableName} ORDER BY ${dataset.datasetConfig.tsKey} LIMIT ${connectorConfig.batchSize} OFFSET $offset"
+      s"SELECT * FROM ${connectorConfig.tableName} ORDER BY ${connectorConfig.timestampColumn} LIMIT ${connectorConfig.batchSize} OFFSET $offset"
     } else {
-      s"SELECT * FROM ${connectorConfig.tableName} WHERE ${dataset.datasetConfig.tsKey} >= '${connectorStats.lastFetchTimestamp}' ORDER BY ${connectorConfig.timestampColumn} LIMIT ${connectorConfig.batchSize} OFFSET $offset"
+      s"SELECT * FROM ${connectorConfig.tableName} WHERE ${connectorConfig.timestampColumn} >= '${connectorStats.lastFetchTimestamp}' ORDER BY ${connectorConfig.timestampColumn} LIMIT ${connectorConfig.batchSize} OFFSET $offset"
     }
   }
 
